3f368a1ed04a37fff3c6969df6644e78df5c6ab0
shelby <shelby.stasny@yahoo.com>
2020-06-05 15:34:02 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListItems from './ListItems'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import * as ReactBootStrap from "react-bootstrap"
 
-function App() {
+library.add(faTrash)
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !==""){
+      const items = [...this.state.items, newItem];
+    this.setState({
+      items: items,
+      currentItem:{
+        text:'',
+        key:''
+      }
+    })
+    }
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  deleteItem(key){
+    const filteredItems= this.state.items.filter(item =>
+      item.key!==key);
+    this.setState({
+      items: filteredItems
+    })
+
+  }
+  setUpdate(text,key){
+    console.log("items:"+this.state.items);
+    const items = this.state.items;
+    items.map(item=>{      
+      if(item.key===key){
+        console.log(item.key +"    "+key)
+        item.text= text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+    
+   
+  }
+ render(){
   return (
+  
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+      <ReactBootStrap.Navbar bg="danger" expand="lg">
+    <ReactBootStrap.Navbar.Brand href="#home">TASK</ReactBootStrap.Navbar.Brand>
+    <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+      <ReactBootStrap.Nav className="mr-auto">
+        <ReactBootStrap.Nav.Link href="#home">Home</ReactBootStrap.Nav.Link>
+        <ReactBootStrap.Nav.Link href="#link">Link</ReactBootStrap.Nav.Link>
+      </ReactBootStrap.Nav>
+    </ReactBootStrap.Navbar.Collapse>
+  </ReactBootStrap.Navbar>
+  </nav>
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+          <button type="submit">Add</button>
+        </form>
+        <p>{this.state.items.text}</p>
+        
+          <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
+        
       </header>
     </div>
   );
+ }
 }
+
 
 export default App;
