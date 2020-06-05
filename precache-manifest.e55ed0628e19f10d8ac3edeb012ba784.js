daa8f67e652204040c01274ca436aecfe37dab64
shelby <shelby.stasny@yahoo.com>
2020-06-04 23:40:18 -0500

    Updates

---
+++ precache-manifest.e55ed0628e19f10d8ac3edeb012ba784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928b4a40273c278de23fef083ccfe34e",
+    "url": "/sstasny/react-todo-list/index.html"
+  },
+  {
+    "revision": "89aebb77254c4bbaac86",
+    "url": "/sstasny/react-todo-list/static/css/main.64584ac1.chunk.css"
+  },
+  {
+    "revision": "c22ce226bc9b079ee3d5",
+    "url": "/sstasny/react-todo-list/static/js/2.b0ebaed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sstasny/react-todo-list/static/js/2.b0ebaed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89aebb77254c4bbaac86",
+    "url": "/sstasny/react-todo-list/static/js/main.9fd5dab2.chunk.js"
+  },
+  {
+    "revision": "a8138846d61c907a1bae",
+    "url": "/sstasny/react-todo-list/static/js/runtime-main.d00e955f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sstasny/react-todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
