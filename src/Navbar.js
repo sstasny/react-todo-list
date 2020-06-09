24bff581f3f2cd71c26a4ddc078f15eb3f75288b
shelby <shelby.stasny@yahoo.com>
2020-06-09 00:47:36 -0500

    changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,13 +9,13 @@ function Navbar() {
            bg-dark">
           <ul className="nav justify-content-center">
   <li className="nav-item">
-    <Link className="nav-link active" to="">Home</Link>
-  </li>
-  <li className="nav-item">
-    <Link className="nav-link" to="/Contacts">Contacts</Link>
+    <Link className="nav-link active" to=""></Link>
   </li>
   <li className="nav-item">
     <Link className="nav-link" to="/Todos">Todos</Link>
+  </li>
+  <li className="nav-item">
+    <Link className="nav-link" to="/Contacts">Contacts</Link>
   </li>
 </ul>
 </nav>
